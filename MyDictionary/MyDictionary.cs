ab4935a99af4925470bff44fbc5af7f375da1d7e
EmineOzbek <77543307+EmineOzbek@users.noreply.github.com>
2021-04-14 01:26:39 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+        public void Add(TKey _key, TValue _value) 
+        {
+            TKey[] tempKeys = _keys;
+            TValue[] tempValues = _values;
+            _keys = new TKey[_keys.Length + 1];
+            _values = new TValue[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+                _values[i] = tempValues[i];
+            }
+            
+            _keys[_keys.Length - 1] = _key;
+            _values[_values.Length - 1] = _value;
+
+        }
+
+        public int Count 
+        {
+            get { return _keys.Length; }
+        }
+        
+        public TKey[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return _values; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> employees = new MyDictionary<int, string>();
+            employees.Add(employees.Count + 1, "Emine");
+            employees.Add(employees.Count + 1, "Nermin");
+            employees.Add(employees.Count + 1, "Oğuzhan");
+            employees.Add(employees.Count + 1, "Elif");
+            employees.Add(employees.Count + 1, "Cemal");
+
+
+
+            for (int i=0; i<employees.Count; i++)
+            {
+                Console.WriteLine("Eleman No: {0}, İsim: {1}", employees.Keys[i], employees.Values[i] );
+            }
+
+            Console.WriteLine("Eleman Sayısın:{0}",employees.Count);
+
+
+
+
+        }
+    }
+}
